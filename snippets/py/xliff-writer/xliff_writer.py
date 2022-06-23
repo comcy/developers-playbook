2793475408b17a2106e32b2c2fe88292989c44fe
comcy <christian.silfang@gmail.com>
2022-06-23 22:03:56 +0200

    added a python xliff sample

--- snippets/py/xliff-writer/xliff_writer.py
+++ snippets/py/xliff-writer/xliff_writer.py
@@ -1,6 +1,10 @@
 from devXliff import XLIFF
 
-obj = XLIFF.XLIFF("sample.txt", 'hi', 'en', True) 
+filename = "projects.component"
+source_lang = "en"
+target_lang= "en"
+
+obj = XLIFF.XLIFF(filename +".ts", source_lang, target_lang, False) 
 #argument1: name of source text file, 
 #argument2: language code for source language
 #argument3: language code for target language
@@ -11,12 +15,16 @@ obj = XLIFF.XLIFF("sample.txt", 'hi', 'en', True)
 
 #Suppose we have a list of source text and target text and we want to save them as XLIFF file so that
 #they can be used by a CAT tool. This can be achieved as follows using devXliff
-list_source=["वह एक अच्छी बेटी है", "मुझे बीकानेर बहुत पसंद है", "अजमेर खूबसूरत अरावली रेंज से घिरा हुआ है"]
-list_target=["She is a good daughter", "I love Bikaner", "Ajmer is surrounded by the beautiful Aravali Range"]
+# list_source=["वह एक अच्छी बेटी है", "मुझे बीकानेर बहुत पसंद है", "अजमेर खूबसूरत अरावली रेंज से घिरा हुआ है"]
+# list_target=["She is a good daughter", "I love Bikaner", "Ajmer is surrounded by the beautiful Aravali Range"]
+
+list_source=["overview", "name", "type"]
+list_target=["overview", "name", "type"]
+
 
 l = list(zip(list_source, list_target)) 
 [obj.add_trans_unit(s,t) for s,t in (l)]  
 
 xliff = obj.get_str()
-with open("sample_XLIFF.xlf", "w", encoding="utf8") as f:
+with open(filename+".xlf", "w", encoding="utf8") as f:
     f.write(xliff)
